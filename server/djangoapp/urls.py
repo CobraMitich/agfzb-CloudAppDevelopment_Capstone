a4c3f933ccfe4372cbd7c8ad7f7df2430562fb5c
CobraMitich <123738355+CobraMitich@users.noreply.github.com>
2023-03-16 12:01:52 +0100

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -3,23 +3,25 @@ from django.conf.urls.static import static
 from django.conf import settings
 from . import views
 
-app_name = 'djangoapp'
+app_name = 'djangoapp' 
 urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
 
-    # path for about view
-    path('about/', views.about, name='about'),    
-    # path for contact us view
+    path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+    path('registration/', views.registration_request, name='registration'),
+    path('login/', views.login_request, name='login'),
+    path('logout/', views.logout_request, name='logout'),
+    path(route='', view=views.get_dealerships, name='index')
     # path for registration
 
     # path for login
 
     # path for logout
 
-    path(route='', view=views.get_dealerships, name='index'),
+    
 
     # path for dealer reviews view
 
